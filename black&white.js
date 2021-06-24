a5bf745fde1d39c7053df91ee591e87e25e9b386
sourabh <sourabhrao77.sr@gmail.com>
2021-06-24 13:32:55 +0530

    Initial commit

--- black&white.js
+++ black&white.js
@@ -1,98 +1,174 @@
-    // nav bar after login 
-    function openNav() {
-        document.getElementById("nav").style.width = "100%";
-    }
+// nav bar after login 
+function openNav() {
+    document.getElementById("nav").style.width = "100%";
+}
 
-    function closeNav() {
-        document.getElementById("nav").style.width = "0%";
-    }
-
-
-
-    /* Open when someone clicks on the span element */
-    function log() {
-        document.getElementById("login").style.width = "100%";
-    }
-
-    /* Close when someone clicks on the "x" symbol inside the overlay */
-    function close1() {
-        document.getElementById("login").style.width = "0%";
-    }
+function closeNav() {
+    document.getElementById("nav").style.width = "0%";
+}
 
 
 
-    // open when someone clicks on home button
-    function home() {
-        document.getElementById("home").style.width = "100%";
-    }
+/* Open when someone clicks on the span element */
+function log() {
+    document.getElementById("login").style.width = "100%";
+}
 
-    /* Close when someone clicks on the "x" symbol inside the overlay */
-    function closehome() {
-        document.getElementById("home").style.width = "0%";
-    }
-
-    // open when someone clicks on search
-    function finding() {
-        document.getElementById("find").style.width = "100%";
-    }
-
-    /* Close when someone clicks on the "x" symbol inside the overlay */
-    function closesearch() {
-        document.getElementById("find").style.width = "0%";
-    }
-    function search(){
-        window.open("https://www.google.com/")
-    }
-
-    //  open when someone clicks on contact
-    function contact() {
-        document.getElementById("detail").style.width = "100%";
-    }
-
-    /* Close when someone clicks on the "x" symbol inside the overlay */
-    function closecontact() {
-        document.getElementById("detail").style.width = "0%";
-    }
-
-    // open when someone clicks on login froms submit button
-
-    function bk() {
-        document.getElementById("home").style.width = "100%";
-    }
-
-    /* Close when someone clicks on the "x" symbol inside the overlay */
-    function closehome() {
-        document.getElementById("home").style.width = "0%";
-    }
-
-    // function for black and white section on home button
-     function openblack() {
-        document.getElementById("black").style.width = "100%";
-
-       
-     }
-     function closeblack() {
-        document.getElementById("black").style.width = "0%";
-    }
-
-      // function for white and white section on home button
-      function openwhite() {
-        document.getElementById("white").style.width = "100%";
-
-       
-     }
-     function closewhite() {
-        document.getElementById("white").style.width = "0%";
-    }
-
-    // when someone clicks on add to cart or buy button
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function close1() {
+    document.getElementById("login").style.width = "0%";
+}
 
 
-    function add(){
-        alert("Added to cart");
 
-    }
+// open when someone clicks on home button
+function home() {
+    document.getElementById("home").style.width = "100%";
+}
 
-    function buy(){
-        alert("Cannot buy now");
-    }
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closehome() {
+    document.getElementById("home").style.width = "0%";
+}
+
+
+// when someone clicks on men 
+function men() {
+    document.getElementById("men").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeoption1() {
+    document.getElementById("men").style.width = "0%";
+}
+
+// when someone clicks on women 
+function women() {
+    document.getElementById("women").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeoption2() {
+    document.getElementById("women").style.width = "0%";
+}
+
+// when someone clicks on kids 
+function kids() {
+    document.getElementById("kids").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeoption3() {
+    document.getElementById("kids").style.width = "0%";
+}
+
+
+
+// open when someone clicks on search
+function finding() {
+    document.getElementById("find").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closesearch() {
+    document.getElementById("find").style.width = "0%";
+}
+function search() {
+    window.open("https://www.google.com/")
+}
+
+//  open when someone clicks on contact
+function contact() {
+    document.getElementById("detail").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closecontact() {
+    document.getElementById("detail").style.width = "0%";
+}
+
+
+
+// open when someone clicks on login froms submit button
+
+function bk() {
+    document.getElementById("home").style.width = "100%";
+}
+
+
+
+/* Close when someone clicks on the "x" on home  */
+function closehome() {
+    document.getElementById("home").style.width = "0%";
+}
+
+
+// function for black and white section on home button
+function blackmen() {
+    document.getElementById("blackmen").style.width = "100%";
+}
+function closebmen() {
+    document.getElementById("blackmen").style.width = "0%";
+}
+
+
+
+// function for black and white section on home button
+function blackwomen() {
+    document.getElementById("blackwomen").style.width = "100%";
+}
+function closebwomen() {
+    document.getElementById("blackwomen").style.width = "0%";
+}
+
+
+// function for black and white section on home button
+function blackkids() {
+    document.getElementById("blackkids").style.width = "100%";
+
+
+}
+function closebkids() {
+    document.getElementById("blackkids").style.width = "0%";
+}
+
+// function for white and white section on home button
+function whitemen() {
+    document.getElementById("whitemen").style.width = "100%";
+}
+function closemen() {
+    document.getElementById("whitemen").style.width = "0%";
+}
+
+
+// function for white and white section on home button
+function whitewomen() {
+    document.getElementById("whitewomen").style.width = "100%";
+}
+function closewomen() {
+    document.getElementById("whitewomen").style.width = "0%";
+}
+
+
+
+// function for white and white section on home button
+function whitekids() {
+    document.getElementById("whitekids").style.width = "100%";
+}
+function closekids() {
+    document.getElementById("whitekids").style.width = "0%";
+}
+
+
+
+// when someone clicks on add to cart or buy button
+
+
+function add() {
+    alert("Added to cart");
+
+}
+
+function buy() {
+    alert("Cannot buy now");
+}
